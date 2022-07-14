0ecc2fa9d5bc31aea858402d2bb53e44fb06aaad
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2022-07-14 03:29:47 +0100

    querying transactions

--- x/nameservice/keeper/msg_server_delete_name.go
+++ x/nameservice/keeper/msg_server_delete_name.go
@@ -1,17 +1,26 @@
 package keeper
 
+// x/nameservice/keeper/msg_server_delete_name.go
 import (
-	"context"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/uwezukwechibuzor/nameservice/x/nameservice/types"
+    "context"
+    "github.com/uwezukwechibuzor/nameservice/x/nameservice/types"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) DeleteName(goCtx context.Context, msg *types.MsgDeleteName) (*types.MsgDeleteNameResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
-	return &types.MsgDeleteNameResponse{}, nil
+  ctx := sdk.UnwrapSDKContext(goCtx)
+  // Try getting name information from the store
+  whois, isFound := k.GetWhois(ctx, msg.Name)
+  // If a name is not found, throw an error
+  if !isFound {
+    return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Name doesn't exist")
+  }
+  // If the message sender address doesn't match the name owner, throw an error
+  if !(whois.Owner == msg.Creator) {
+    return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner")
+  }
+  // Otherwise, remove the name information from the store
+  k.RemoveWhois(ctx, msg.Name)
+  return &types.MsgDeleteNameResponse{}, nil
 }
